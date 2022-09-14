b321d4e9ed7a20fe5e1b7b2524143bc7db9dde33
rodrigoperez123 <rixcotoyacp1@miumg.edu.gt>
2022-09-14 17:23:24 -0600

    Se Agrego el Proyecto

---
+++ Grupo_2/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace Grupo_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle28 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle29 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle30 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvAdministrador = new MetroFramework.Controls.MetroGrid();
+            this.dgvProcesos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvFisica = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvVirtual = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvCPU = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnActualizar = new MetroFramework.Controls.MetroTile();
+            this.btnDetener = new MetroFramework.Controls.MetroTile();
+            this.btnSalir = new MetroFramework.Controls.MetroTile();
+            this.txtcontador = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProceso = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAdministrador)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvAdministrador
+            // 
+            this.dgvAdministrador.AllowUserToResizeRows = false;
+            this.dgvAdministrador.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dgvAdministrador.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvAdministrador.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dgvAdministrador.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle28.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle28.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(17)))), ((int)(((byte)(65)))));
+            dataGridViewCellStyle28.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle28.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle28.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(19)))), ((int)(((byte)(73)))));
+            dataGridViewCellStyle28.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle28.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvAdministrador.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle28;
+            this.dgvAdministrador.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAdministrador.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvProcesos,
+            this.dgvID,
+            this.dgvFisica,
+            this.dgvVirtual,
+            this.dgvCPU});
+            dataGridViewCellStyle29.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle29.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle29.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle29.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle29.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(19)))), ((int)(((byte)(73)))));
+            dataGridViewCellStyle29.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle29.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvAdministrador.DefaultCellStyle = dataGridViewCellStyle29;
+            this.dgvAdministrador.EnableHeadersVisualStyles = false;
+            this.dgvAdministrador.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.dgvAdministrador.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dgvAdministrador.Location = new System.Drawing.Point(43, 82);
+            this.dgvAdministrador.Name = "dgvAdministrador";
+            this.dgvAdministrador.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle30.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle30.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(17)))), ((int)(((byte)(65)))));
+            dataGridViewCellStyle30.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle30.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle30.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(19)))), ((int)(((byte)(73)))));
+            dataGridViewCellStyle30.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle30.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvAdministrador.RowHeadersDefaultCellStyle = dataGridViewCellStyle30;
+            this.dgvAdministrador.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgvAdministrador.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvAdministrador.Size = new System.Drawing.Size(867, 509);
+            this.dgvAdministrador.Style = MetroFramework.MetroColorStyle.Red;
+            this.dgvAdministrador.TabIndex = 0;
+            this.dgvAdministrador.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvAdministrador_CellContentClick);
+            // 
+            // dgvProcesos
+            // 
+            this.dgvProcesos.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dgvProcesos.HeaderText = "Proceso";
+            this.dgvProcesos.Name = "dgvProcesos";
+            // 
+            // dgvID
+            // 
+            this.dgvID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dgvID.HeaderText = "ID";
+            this.dgvID.Name = "dgvID";
+            // 
+            // dgvFisica
+            // 
+            this.dgvFisica.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dgvFisica.HeaderText = "Memoria Fisica";
+            this.dgvFisica.Name = "dgvFisica";
+            // 
+            // dgvVirtual
+            // 
+            this.dgvVirtual.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dgvVirtual.HeaderText = "Memoria Virtual";
+            this.dgvVirtual.Name = "dgvVirtual";
+            // 
+            // dgvCPU
+            // 
+            this.dgvCPU.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dgvCPU.HeaderText = "CPU";
+            this.dgvCPU.Name = "dgvCPU";
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.ActiveControl = null;
+            this.btnActualizar.Location = new System.Drawing.Point(600, 616);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(82, 37);
+            this.btnActualizar.TabIndex = 1;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseSelectable = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.ActiveControl = null;
+            this.btnDetener.Location = new System.Drawing.Point(713, 617);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(82, 36);
+            this.btnDetener.Style = MetroFramework.MetroColorStyle.Orange;
+            this.btnDetener.TabIndex = 2;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseSelectable = true;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.ActiveControl = null;
+            this.btnSalir.Location = new System.Drawing.Point(826, 616);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(84, 37);
+            this.btnSalir.Style = MetroFramework.MetroColorStyle.Green;
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseSelectable = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // txtcontador
+            // 
+            this.txtcontador.AutoSize = true;
+            this.txtcontador.Location = new System.Drawing.Point(72, 617);
+            this.txtcontador.Name = "txtcontador";
+            this.txtcontador.Size = new System.Drawing.Size(50, 13);
+            this.txtcontador.TabIndex = 4;
+            this.txtcontador.Text = "Contador";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 652);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nombre del proceso:";
+            // 
+            // txtProceso
+            // 
+            this.txtProceso.AutoSize = true;
+            this.txtProceso.Location = new System.Drawing.Point(190, 652);
+            this.txtProceso.Name = "txtProceso";
+            this.txtProceso.Size = new System.Drawing.Size(13, 13);
+            this.txtProceso.TabIndex = 6;
+            this.txtProceso.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 99999999;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 695);
+            this.Controls.Add(this.txtProceso);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtcontador);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.dgvAdministrador);
+            this.Name = "Form1";
+            this.Style = MetroFramework.MetroColorStyle.Red;
+            this.Text = "Administrador_De_Tareas";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAdministrador)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroGrid dgvAdministrador;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvProcesos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvFisica;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvVirtual;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvCPU;
+        private MetroFramework.Controls.MetroTile btnActualizar;
+        private MetroFramework.Controls.MetroTile btnDetener;
+        private MetroFramework.Controls.MetroTile btnSalir;
+        private System.Windows.Forms.Label txtcontador;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtProceso;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Grupo_2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grupo_2
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateProcessList();
+            timer1.Enabled = true;
+
+        }
+        private void UpdateProcessList() {
+            dgvAdministrador.Rows.Clear();
+            foreach (Process p in Process.GetProcesses()){
+                int n = dgvAdministrador.Rows.Add();
+                dgvAdministrador.Rows[n].Cells[0].Value = p.ProcessName;
+                dgvAdministrador.Rows[n].Cells[1].Value = p.Id;
+                dgvAdministrador.Rows[n].Cells[2].Value = p.WorkingSet64;
+                dgvAdministrador.Rows[n].Cells[3].Value = p.VirtualMemorySize64;
+                dgvAdministrador.Rows[n].Cells[4].Value = p.SessionId;
+            }
+            txtcontador.Text = "Procesos Actuales: " + dgvAdministrador.Rows.Count.ToString();
+            }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (Process p in Process.GetProcesses())
+                {
+                    if (p.ProcessName == txtProceso.Text)
+                    {
+                        p.Kill(); // elimina el proceso
+                    }
+                }
+            }
+            catch (Exception x)
+            { 
+            MessageBox.Show(" no selecciono nigun proceso " + x, " error al eliminar ", MessageBoxButtons.OK);
+            }
+    }
+        
+
+        private void dgvAdministrador_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtProceso.Text = dgvAdministrador.CurrentRow.Cells[0].Value.ToString();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            UpdateProcessList();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateProcessList();
+        }
+    }
+}
